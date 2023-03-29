027ba74f12ee1e40eaa5aacb29f21b9bb8e779e9
Bengley123 <120467773+Bengley123@users.noreply.github.com>
2023-03-29 17:07:12 +0700

    menambahkan commit baru

--- modul6_1302213108/Program.cs
+++ modul6_1302213108/Program.cs
@@ -7,6 +7,15 @@ class Program
     static void Main(string[] args)
     {
         SayaTubeUser Yesa = new SayaTubeUser("Yesa");
-
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
+        Yesa.AddVideo(new SayaTubeUser("Review Film John Wick 4"));
     }
 }
